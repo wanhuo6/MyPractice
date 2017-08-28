5a8b96673ef818b76416579d498fe1f969eab8ce
liuhuijie <857166619@qq.com>
2017-08-28 19:39:57 +0800

    添加园表

--- app/src/main/java/com/ahuo/practise/MainActivity.java
+++ app/src/main/java/com/ahuo/practise/MainActivity.java
@@ -1,13 +1,36 @@
 package com.ahuo.practise;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ahuo.practise.widget.CircleView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int mProgress;
+
+    private CircleView mCircleView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCircleView= (CircleView) findViewById(R.id.circleView);
+        mHandler.sendEmptyMessageDelayed(11, 50);
     }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (mProgress>100){
+                mProgress=0;
+            }
+            mCircleView.setProgress(mProgress);
+            mProgress++;
+            mHandler.sendEmptyMessageDelayed(11, 50);
+        }
+    };
 }
--- app/src/main/java/com/ahuo/practise/widget/CircleView.java
+++ app/src/main/java/com/ahuo/practise/widget/CircleView.java
@@ -1,24 +1,108 @@
 package com.ahuo.practise.widget;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.ahuo.practise.R;
 
 /**
  * Created by ahuo on 17-8-28.
  */
 
 public class CircleView extends View {
+
+    private Context mContext;
+
+    private Paint mCirClePaint;
+
+    private Paint mSwingPaint;
+
+    private int mProgress;
+
+    private int mWidth = 500;
+
+    private int mHeight = 500;
+
     public CircleView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        initView();
     }
+
+    private void initView() {
+        mContext = getContext();
+        mCirClePaint = new Paint();
+        mCirClePaint.setColor(mContext.getResources().getColor(R.color.colorAccent));
+        mCirClePaint.setAntiAlias(true);
+        mCirClePaint.setStrokeWidth(3);
+        mCirClePaint.setStrokeCap(Paint.Cap.ROUND);
+        mCirClePaint.setStyle(Paint.Style.STROKE);
+
+        mSwingPaint = new Paint();
+
+        mSwingPaint.setColor(mContext.getResources().getColor(R.color.colorPrimaryDark));
+        mSwingPaint.setAntiAlias(true);
+        mSwingPaint.setStrokeWidth(3);
+        mSwingPaint.setStrokeCap(Paint.Cap.ROUND);
+        mSwingPaint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //刻度线
+        canvas.save();
+        for (int i = 0; i < 100; i++) {
+            if (mProgress > i) {
+                mCirClePaint.setColor(mContext.getResources().getColor(R.color.colorAccent));
+            } else {
+                mCirClePaint.setColor(mContext.getResources().getColor(R.color.colorPrimary));
+            }
+            canvas.drawLine(mWidth / 2, 0, mWidth / 2, 50, mCirClePaint);
+            // 旋转的度数 = 100 / 360
+            canvas.rotate(3.6f, mWidth / 2, mHeight / 2);
+        }
+        canvas.restore();
+        canvas.save();
+        canvas.drawLine(mWidth / 2, mWidth / 2, mWidth / 2, -600, mSwingPaint);
+        canvas.restore();
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        final int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        final int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        final int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        final int heightSpecSize = MeasureSpec.getMode(heightMeasureSpec);
+        if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(mWidth, mHeight);
+        } else if (widthSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(mWidth, heightSpecSize);
+        } else if (heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(widthSpecSize, mHeight);
+        }
+    }
+
+    /**
+     * 设置进度
+     */
+    public void setProgress(int progress) {
+        this.mProgress = progress;
+        invalidate();
+    }
+
 }
